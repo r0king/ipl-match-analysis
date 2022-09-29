76c00021ad94843b897755d9bf883ad413090730
kng <kng@pop-os.localdomain>
2022-09-29 12:11:46 +0530

    deploy error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import "animate.css/animate.min.css";
 import Card from './componets/Card';
+import Hero from './componets/Hero';
 function App() {
   return (
   
   <div>
-    <h1 className="animate__animated animate__fadeInLeft animate__delay-faster ">An animated element</h1>
-    <Card className="animate__animated animate__fadeInLeft animate__delay-faster " ></Card>
+    <Hero ></Hero>
   </div>
   );
 }
---
+++ src/componets/Hero.js
@@ -0,0 +1,33 @@
+// import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export class Hero extends Component {
+    constructor(props){
+        super()
+    }
+    
+  render() {
+    return (
+      <div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+              src="https://placeimg.com/260/400/arch"
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div className="animate__animated animate__fadeInLeft animate__delay-faster">
+              <h1 className="text-5xl font-bold">IPL News!</h1>
+              <p className="py-6">
+            Get the stats of the previous games in the
+IPL, so that you can get to know about the stats in a click and back your die-hard fanship in the game.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
